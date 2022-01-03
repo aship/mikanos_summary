a6613bf6865f417c709d2de692f5d621065f6df6
Yasuo Ashina <ashipp3000@gmail.com>
2022-01-03 14:52:11 +0900

    day16a 16.1 ターミナルでキー入力

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -209,7 +209,6 @@ extern "C" void KernelMainNewStack(
     .InitContext(TaskB, 45)
     .Wakeup()
     .ID();
-
   const uint64_t task_terminal_id = task_manager->NewTask()
     .InitContext(TaskTerminal, 0)
     .Wakeup()
@@ -270,9 +269,18 @@ extern "C" void KernelMainNewStack(
           printk("wakeup TaskB: %s\n", task_manager->Wakeup(taskb_id).Name());
         }
       } else {
-        printk("key push not handled: keycode %02x, ascii %02x\n",
-            msg->arg.keyboard.keycode,
-            msg->arg.keyboard.ascii);
+        __asm__("cli");
+        auto task_it = layer_task_map->find(act);
+        __asm__("sti");
+        if (task_it != layer_task_map->end()) {
+          __asm__("cli");
+          task_manager->SendMessage(task_it->second, *msg);
+          __asm__("sti");
+        } else {
+          printk("key push not handled: keycode %02x, ascii %02x\n",
+              msg->arg.keyboard.keycode,
+              msg->arg.keyboard.ascii);
+        }
       }
       break;
     case Message::kLayer:
